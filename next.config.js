d1de616b77e98a15ac6f1d52925285246c86c27a
unknown <vaishnavisharma564@gmail.com>
2026-05-05 12:59:05 +0530

    Make config conditional for Vercel vs GitHub Pages

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  output: 'export',
-  basePath: '/quirkbyte-chronicles',
-  trailingSlash: true,
+  ...(process.env.VERCEL ? {} : {
+    output: 'export',
+    basePath: '/quirkbyte-chronicles',
+    trailingSlash: true,
+  }),
 }
 
 module.exports = withMDX(nextConfig)
