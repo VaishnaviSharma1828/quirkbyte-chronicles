a6324218505af27d274a10b38c9cac068b83af7d
unknown <vaishnavisharma564@gmail.com>
2026-05-05 12:26:02 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  output: 'export',
+  basePath: '/quirkbyte-chronicles',
+  trailingSlash: true,
 }
 
 module.exports = withMDX(nextConfig)
